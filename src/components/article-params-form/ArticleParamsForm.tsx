bf8ac3d761afe9ade265d4846997d2f5d1d38d1d
anykey <billykidd@mail.ru>
2024-10-16 21:02:35 +0300

    изменения

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,37 @@
+import { ArticleStateType } from 'src/constants/articleProps';
+import styles from './ArticleParamsForm.module.scss';
+import clsx from 'clsx';
+
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
+import { useRef, useState } from 'react';
 
-import styles from './ArticleParamsForm.module.scss';
+type ArticleParamsFormProps = {
+	setCurrentArticleState: (param: ArticleStateType) => void;
+	currentArticleState: ArticleStateType;
+};
 
-export const ArticleParamsForm = () => {
+export const ArticleParamsForm = ({}: // currentArticleState,
+// setCurrentArticleState,
+ArticleParamsFormProps) => {
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const rootRef = useRef<HTMLDivElement>(null);
+	// const[selectArticleState, setSelectArticleState] = useState<ArticleStateType>(currentArticleState);
+
+	useOutsideClickClose({
+		isOpen,
+		rootRef,
+		onClose: () => setIsOpen(false),
+		onChange: setIsOpen,
+		event: 'mousedown',
+	});
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpen} onClick={setIsOpen} />
+			<aside
+				className={clsx(styles.container, isOpen && styles.container_open)}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
